b09bbf08eddfdf0a149d961e163b59f82c02a864
Toria <tzimms7@gmail.com>
2019-10-15 18:06:07 +0100

    my travels

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Travel from "./Travel";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">My Travels</h1>
+        </header>
+        <Travel
+          destination="Phuket"
+          country="Thailand"
+          photo="https://cdn.pixabay.com/photo/2017/02/21/00/54/beach-2084350_1280.jpg"
+          
+          distance=  "10,941 km"
+        />
+        <Travel
+          destination="Bali"
+          country="Indonesia"
+          photo="https://images.pexels.com/photos/1814376/pexels-photo-1814376.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+          distance= "13,520 km"
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
